1252d5633d608f75e61e978a4125e26c31fd0943
FedeGalli <federicogalli96@gmail.com>
2024-07-30 16:14:53 +0200

    Added Resume, Project and contact sections

--- scripts/animations.js
+++ scripts/animations.js
@@ -7,22 +7,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const observerCallback = (entries, observer) => {
         entries.forEach(entry => {
+
             if (entry.isIntersecting) {
+
                 entry.target.classList.add('visible');
                 observer.unobserve(entry.target);
             }
+
         });
     };
 
     const observer = new IntersectionObserver(observerCallback, observerOptions);
 
-    const targets = document.querySelectorAll('.animate-on-scroll');
+    const targets = document.querySelectorAll('.resume-item');
     targets.forEach(target => {
         observer.observe(target);
     });
 
-    const targets1 = document.querySelectorAll('.section-header');
+    const targets2 = document.querySelectorAll('.education-item');
+    targets2.forEach(target => {
+        observer.observe(target);
+    });
+
+    const targets1 = document.querySelectorAll('.section-header-resume');
     targets1.forEach(target => {
         observer.observe(target);
     });
+
+    const targets3 = document.querySelectorAll('.section-header-education');
+    targets3.forEach(target => {
+        observer.observe(target);
+    });
+
+    const targets4 = document.querySelectorAll('.section-header-projects');
+    targets4.forEach(target => {
+        observer.observe(target);
+    });
+
+    const targets5 = document.querySelectorAll('.image-tile');
+    targets5.forEach(target => {
+        observer.observe(target);
+    });
+
+
 });
